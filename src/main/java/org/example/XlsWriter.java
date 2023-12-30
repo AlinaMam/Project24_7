4dff05ea97d60ad7c03eedf6bda66eff4b08a2c1
Alina <alinaamamontova@gmail.com>
2023-12-30 09:21:57 +0300

    добавлено закрытие потока

--- src/main/java/org/example/XlsWriter.java
+++ src/main/java/org/example/XlsWriter.java
@@ -74,8 +74,8 @@ public class XlsWriter {
                 }
             }
         }
-        try {
-            FileOutputStream fos = new FileOutputStream(path);
+
+        try (FileOutputStream fos = new FileOutputStream(path)) {
             wb.write(fos);
         } catch (IOException e) {
             e.printStackTrace();
